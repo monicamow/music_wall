c71883b94d307168954a7ef3d9ebfeb1699899e6
monicamow <mow.monica@gmail.com>
2016-06-14 09:11:33 -0700

    tracks view now shows vote count

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,8 @@ class Vote < ActiveRecord::Base
   belongs_to :track
   belongs_to :user
 
+  # make validationg for user only being able to vote once for one track
+
+  # get all users votes...check to see if the track is in there
+
 end
